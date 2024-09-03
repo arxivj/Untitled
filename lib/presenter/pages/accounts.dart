175c753fe9b04e0601f6897a320014dc38702ab4
arxivj <xivpark1110@gmail.com>
2024-09-04 01:30:12 +0900

    Feat: AccountsWidget에 AccountDetailsCard 및 TradeHistoryCard 추가
    - TradeHistoryCard 위젯은 예상되는 형태로 예제 위젯을 구현
    - AccountDetailsCard 위젯은 실제로 사용될 계좌 상세 정보 표시용으로 구현

---
+++ lib/presenter/pages/accounts.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/presenter/widgets/accounts/account_detail_card.dart';
+import 'package:untitled/presenter/widgets/accounts/accounts_trade_history_card.dart';
+import 'package:untitled/utils/constants.dart';
+
+class AccountsWidget extends StatelessWidget {
+  const AccountsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final tradeHistory = TradeHistory(
+      stockName: 'Apple',
+      stockSymbol: 'AAPL',
+      tradePrice: '-20,000원',
+      tradeChange: '-1.5%',
+    );
+
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Spacing.mediumHeight(),
+          const Padding(
+            padding: AppPadding.kHorizontalPadding,
+            child: AccountDetailsCard(),
+          ),
+          Spacing.largeHeight(),
+          TradeHistoryCard(tradeHistory: tradeHistory),
+          Spacing.extraLargeHeight(),
+        ],
+      ),
+    );
+  }
+}
