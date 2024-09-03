f2b8d4341591065c5066176b3f6a5977ae5a93fe
arxivj <xivpark1110@gmail.com>
2024-09-04 00:25:35 +0900

    Rrefactor: 상수화된 간격 및 패딩 클래스를 추가하여 코드 일관성 개선
    - `AppPadding` 클래스를 추가하여 공통 패딩 값을 상수화
    - `AppSizes` 클래스를 추가하여 공통 크기 값을 상수화
    - `Spacing` 클래스를 추가하여 자주 사용되는 간격을 상수화하고, `SizedBox` 위젯을 생성하는 메서드 제공

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -9,3 +9,24 @@ class AppPadding {
 class AppSizes {
   static const Size kAppBarHeight = Size.fromHeight(56.0);
 }
+
+class Spacing {
+  static const double small = 8.0;
+  static const double medium = 16.0;
+  static const double large = 32.0;
+  static const double extraLarge = 40.0;
+  static const double appBarHeight = 56.0;
+
+  static Widget height(double size) => SizedBox(height: size);
+  static Widget width(double size) => SizedBox(width: size);
+
+  static Widget smallHeight() => height(small);
+  static Widget mediumHeight() => height(medium);
+  static Widget largeHeight() => height(large);
+  static Widget extraLargeHeight() => height(extraLarge);
+  static Widget appBarHeightSpace() => height(appBarHeight);
+
+  static Widget smallWidth() => width(small);
+  static Widget mediumWidth() => width(medium);
+  static Widget largeWidth() => width(large);
+}
