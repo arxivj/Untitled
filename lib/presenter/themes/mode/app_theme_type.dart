6723bf845cbfc23d91b209022dacce9a7dce0273
arxivj <xivpark1110@gmail.com>
2024-09-05 13:48:26 +0900

    Chore: 불필요한 AppThemeType enum 제거 및 ThemeMode로 대체

--- lib/presenter/themes/mode/app_theme_type.dart
+++
@@ -1,4 +0,0 @@
-enum AppThemeType {
-  light,
-  dark,
-}
--- lib/presenter/themes/mode/light_app_theme.dart
+++ lib/presenter/themes/mode/light_app_theme.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:untitled/presenter/themes/app_theme.dart';
 import 'package:untitled/presenter/themes/app_theme_styles.dart';
 import 'package:untitled/presenter/themes/colors/app_theme_colors.dart';
-import 'package:untitled/presenter/themes/mode/app_theme_type.dart';
 
 class LightAppTheme extends AppTheme {
   LightAppTheme()
       : super(
-          name: AppThemeType.light.name,
+          name: ThemeMode.light.name,
           brightness: Brightness.light,
           colors: AppThemeColors.fromAppColors(isDarkMode: false),
           styles: const AppThemeStyles(),
