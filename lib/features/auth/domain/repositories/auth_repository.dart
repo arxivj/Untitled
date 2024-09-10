843013b9bb8f0c068c2ebae2d2fbe562f4e0ec36
arxivj <xivpark1110@gmail.com>
2024-09-10 16:12:14 +0900

    Refactor: interface 제한자 적용 및 abstract와 함께 사용하여 상속 방지 (AuthRepository)

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,7 +2,7 @@ import 'package:untitled/core/enums/platform_enum.dart';
 import 'package:untitled/features/auth/domain/entities/auth_user_entity.dart';
 import 'package:untitled/features/auth/domain/entities/token.dart';
 
-abstract class AuthRepository {
+abstract interface class AuthRepository {
   Future<AuthUserEntity> login(PlatformEnum platform);
   Future<List<Token>> requestToken(AuthUserEntity user);
 }
