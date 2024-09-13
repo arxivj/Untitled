939c54f1f32e7ec4ea9e4ed15759b7b7f527867e
arxivj <xivpark1110@gmail.com>
2024-09-13 20:46:23 +0900

    AuthRepository import 수정

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,8 +1,8 @@
 import 'package:untitled/core/enums/platform_enum.dart';
-import 'package:untitled/features/auth/domain/entities/auth_user_entity.dart';
 import 'package:untitled/features/auth/domain/entities/token.dart';
+import 'package:untitled/features/auth/domain/entities/user_entity.dart';
 
 abstract interface class AuthRepository {
-  Future<AuthUserEntity> login(PlatformEnum platform);
-  Future<List<Token>> requestToken(AuthUserEntity user);
+  Future<UserEntity> login(PlatformEnum platform);
+  Future<List<Token>> requestToken(UserEntity user);
 }
