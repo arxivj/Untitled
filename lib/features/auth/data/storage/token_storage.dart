de78bbbc61379b09a9f20ac534fc368483156963
arxivj <xivpark1110@gmail.com>
2024-09-24 21:04:36 +0900

    refactor: 로컬 저장소 키 관리를 위해 LocalStorageTokenType enum을 사용하도록 변경

--- lib/features/auth/data/storage/token_storage.dart
+++ lib/features/auth/data/storage/token_storage.dart
@@ -2,22 +2,31 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:untitled/core/enums/auth_token_type.dart';
 import 'package:untitled/features/auth/domain/entities/token.dart';
 
+enum LocalStorageTokenType {
+  accessToken('access_token'),
+  refreshToken('refresh_token');
+
+  final String storageKey;
+
+  const LocalStorageTokenType(this.storageKey);
+}
+
 class TokenStorage {
   Future<void> saveTokens(List<Token> tokens) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    final accessToken = tokens.firstWhere((token) => token.type == AuthTokenType.accessToken);
-    final refreshToken = tokens.firstWhere((token) => token.type == AuthTokenType.refreshToken);
+    final accessToken = tokens.firstWhere((token) => token.type == AuthTokenType.accessToken).token;
+    final refreshToken = tokens.firstWhere((token) => token.type == AuthTokenType.refreshToken).token;
 
-    await prefs.setString(AuthTokenType.accessToken.jsonKey, accessToken.token);
-    await prefs.setString(AuthTokenType.refreshToken.jsonKey, refreshToken.token);
+    await prefs.setString(LocalStorageTokenType.accessToken.storageKey, accessToken);
+    await prefs.setString(LocalStorageTokenType.refreshToken.storageKey, refreshToken);
   }
 
   Future<List<Token>> loadTokens() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    final accessToken = prefs.getString(AuthTokenType.accessToken.jsonKey);
-    final refreshToken = prefs.getString(AuthTokenType.refreshToken.jsonKey);
+    final accessToken = prefs.getString(LocalStorageTokenType.accessToken.storageKey);
+    final refreshToken = prefs.getString(LocalStorageTokenType.refreshToken.storageKey);
 
     if (accessToken != null && refreshToken != null) {
       return [
@@ -31,7 +40,7 @@ class TokenStorage {
 
   Future<void> clearTokens() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.remove(AuthTokenType.accessToken.jsonKey);
-    await prefs.remove(AuthTokenType.refreshToken.jsonKey);
+    await prefs.remove(LocalStorageTokenType.accessToken.storageKey);
+    await prefs.remove(LocalStorageTokenType.refreshToken.storageKey);
   }
 }
