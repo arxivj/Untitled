6942618525b67c8ba553f151db35069175f9f2d0
arxivj <xivpark1110@gmail.com>
2024-09-24 22:02:16 +0900

    Remove: TokenDTO 내 불필요한 메서드 제거

--- lib/features/auth/data/models/token_dto.dart
+++ lib/features/auth/data/models/token_dto.dart
@@ -1,6 +1,3 @@
-import 'package:untitled/core/enums/auth_token_type.dart';
-import 'package:untitled/features/auth/domain/entities/token_entity.dart';
-
 class TokenDTO {
   final String accessToken;
   final String refreshToken;
@@ -9,18 +6,4 @@ class TokenDTO {
     required this.accessToken,
     required this.refreshToken,
   });
-
-  factory TokenDTO.fromJson(Map<String, dynamic> json) {
-    return TokenDTO(
-      accessToken: json[AuthTokenType.accessToken.jsonKey],
-      refreshToken: json[AuthTokenType.refreshToken.jsonKey],
-    );
-  }
-
-  List<TokenEntity> toDomain() {
-    return [
-      TokenEntity(token: accessToken, type: AuthTokenType.accessToken),
-      TokenEntity(token: refreshToken, type: AuthTokenType.refreshToken),
-    ];
-  }
 }
