892635d1d1d64707425b33fc02d17364ae3a97b9
arxivj <xivpark1110@gmail.com>
2024-09-27 01:54:02 +0900

    refactor: AuthPlatform enum 타입을 활용하도록 변경

--- lib/features/auth/data/models/email_password_user_dto.dart
+++ lib/features/auth/data/models/email_password_user_dto.dart
@@ -1,3 +1,4 @@
+import 'package:untitled/core/enums/auth_platform.dart';
 import 'package:untitled/core/enums/auth_user_field.dart';
 import 'package:untitled/features/auth/data/models/user_dto.dart';
 
@@ -14,9 +15,10 @@ class EmailPasswordUserDTO extends UserDTO {
   }
 
   factory EmailPasswordUserDTO.fromJson(Map<String, dynamic> json) {
+    final AuthPlatform platform = AuthPlatform.fromId(json[AuthUserField.platform.jsonKey]);
     return EmailPasswordUserDTO(
       email: json[AuthUserField.email.jsonKey],
-      platform: json[AuthUserField.platform.jsonKey],
+      platform: platform,
     );
   }
 }
